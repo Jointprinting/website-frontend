316ae2e23125c3a4a25aeafa1af50bfaed6610a8
alveiz <alvin.eizner@vanderbilt.edu>
2024-04-14 13:08:56 -0400

    updated about page

--- src/screens/About.js
+++ src/screens/About.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Box, Stack } from '@mui/material';
 import Typography from '../modules/components/Typography';
 import Avatar from '@mui/material/Avatar';
+import storyImage from '../modules/images/storyImage.webp'
 
 function About() {
   return (
@@ -50,17 +51,21 @@ function About() {
             <Typography align="center" variant="h2" >
                 Our Story
             </Typography>
-            <Stack direction="row" alignItems="center" justifyContent="center" spacing={10} pt={6}>
-                <img src='https://fp.freshprints.com/assets/icons/about-us/our_story_girl_new.png' alt='story' style={{height:'30vw', width: 'auto'}} />
-                <Stack spacing={1} alignItems="center" maxWidth="35vw">
-                    <Typography align="center" variant="h4">From Collegiate to Corporate</Typography>
-                    <Typography>We’ve been making trendy merch for college students since 2013, but never thought much about the corporate market. Then we noticed that the handful of businesses that did work with us always stuck around… for years.
-
-We started with: why do they like working with us?
-
-Today we’re at: we get it and we’ve leveled up.
-
-We now stand by our ability to create high-quality and on-brand merch for students and companies alike. No more crappy t-shirts or cheap pens that people toss immediately. We put your logo on merch that means something. Merch your community will proudly wear.</Typography>
+            <Stack direction="row" alignItems="center" justifyContent="center" spacing={10} pt={8}>
+                <img src={storyImage} alt='story' style={{ width: '27vw', height: 'auto'}} />
+                <Stack spacing={2} alignItems="center" maxWidth="35vw">
+                    <Typography align="center" variant="h4">Rooted in Excellence</Typography>
+                    <Typography>At Joint Printing, our roots are grounded in the belief that there's always room for innovation.</Typography>
+                    <Typography>
+                        Our story began when the desire to fully realize our potential outgrew the confines of our previous roles. 
+                        Fueled by the conviction that we could offer more, we embarked on an entrepreneurial voyage, crafting a company that's as dynamic and forward-thinking as the clients we serve. 
+                    </Typography>
+                    <Typography>
+                        We're not just another printing service; we're a collective of visionaries committed to bringing your brand’s story into the physical world with unmatched quality and precision.
+                    </Typography>
+                    <Typography>
+                        Here at Joint Printing, your vision is entrusted to a team that cares deeply about bringing it to life, ensuring every detail is not just printed, but imprinted with excellence.
+                    </Typography>
                 </Stack>
                 
             </Stack>
@@ -68,20 +73,19 @@ We now stand by our ability to create high-quality and on-brand merch for studen
 
         <Box pt="5vh" pb="20vh">
             <Typography align="center" variant="h2" >
-                Awesome Values
+                Mission & Values
             </Typography>
             <Stack direction="row" alignItems="center" justifyContent="center" spacing={10} pt={6}>
-                <Stack spacing={1} alignItems="center" maxWidth="35vw">
-                    <Typography align="center" variant="h4">From Collegiate to Corporate</Typography>
-                    <Typography>We’ve been making trendy merch for college students since 2013, but never thought much about the corporate market. Then we noticed that the handful of businesses that did work with us always stuck around… for years.
+                <Stack spacing={2} alignItems="start" maxWidth="35vw">
+                    <Typography variant="h4">Our Mission</Typography>
+                    <Typography sx={{pb: 2}}>To elevate brands with stellar print solutions and high-quality apparel that blends craftsmanship with convenience, giving you clothing that fully encapsulates your company's spirit.</Typography>
 
-We started with: why do they like working with us?
-
-Today we’re at: we get it and we’ve leveled up.
-
-We now stand by our ability to create high-quality and on-brand merch for students and companies alike. No more crappy t-shirts or cheap pens that people toss immediately. We put your logo on merch that means something. Merch your community will proudly wear.</Typography>
+                    <Typography variant="h4" >Our Values</Typography>
+                    <Typography>At Joint Printing, we're all about sparking joy in the print process. We’re in the business of making connections — not just between suppliers and clients but also between your brand and your audience. 
+                        Think of us as your creative sidekick; we're here to ensure that each print piece not only looks fantastic but also resonates with your brand's playful spirit and ambition.
+                    </Typography>
                 </Stack>
-                <img src='https://fp.freshprints.com/assets/icons/about-us/our_story_girl_new.png' alt='story' style={{height:'30vw', width: 'auto'}} />
+                <img src='https://images.pexels.com/photos/15543047/pexels-photo-15543047/free-photo-of-whiteboard-with-hashtag-company-values-of-walls-io-website.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' alt='story' style={{width:'33vw', height: 'auto'}} />
 
                 
             </Stack>
