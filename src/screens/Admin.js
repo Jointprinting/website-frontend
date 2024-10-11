8c48b93b7d6fdd842a0937c58ca5a70ad9657024
alveiz <alvin.eizner@vanderbilt.edu>
2024-10-10 23:11:47 -0400

    new admin screen
    tsimplified product adding so that you can now input a style code and alphabroder api will take care of the rest

--- src/screens/Admin.js
+++ src/screens/Admin.js
@@ -6,84 +6,17 @@ import config from '../config.json';
 
 function Admin() {
     const mobile = useMediaQuery("(max-width: 800px)");
-    const [name, setName] = React.useState('');
-    const [vendor, setVendor] = React.useState('');
-    const [description, setDescription] = React.useState('');
-    const [sizeRangeBottom, setSizeRangeBottom] = React.useState('');
-    const [sizeRangeTop, setSizeRangeTop] = React.useState('');
+    const [styleCode, setStyleCode] = React.useState('');
     const [priceRangeBottom, setPriceRangeBottom] = React.useState('');
     const [priceRangeTop, setPriceRangeTop] = React.useState('');
-    const [colors, setColors] = React.useState([]);
-    const [colorCodes, setColorCodes] = React.useState([]);
-    const [productFrontImages, setProductFrontImages] = React.useState([]);
-    const [productBackImages, setProductBackImages] = React.useState([]);
-    const [rating, setRating] = React.useState('');
-    const [tag, setTag] = React.useState('');
+    const [rating, setRating] = React.useState(5);
+    const [tag, setTag] = React.useState('Best Seller');
     const [category, setCategory] = React.useState('');
     const [type, setType] = React.useState('');
-    const [colorInputValue, setColorInputValue] = React.useState('');
-    const [colorHexInputValue, setColorHexInputValue] = React.useState('');
-    const [productFrontImagesInputValue, setProductFrontImagesInputValue] = React.useState('');
-    const [productBackImagesInputValue, setProductBackImagesInputValue] = React.useState('');
-
-    const handleAddColor = (event) => {
-        if (event.key === 'Enter' && colorInputValue.trim() !== '') {
-            setColors([...colors, colorInputValue.trim()]);
-            setColorInputValue('');
-        }
-    };
-
-    const handleAddColorHex = (event) => {
-        if (event.key === 'Enter' && colorHexInputValue.trim() !== '') {
-            if(colors.length - 1 >= colorCodes.length) {
-                setColorCodes([...colorCodes, colorHexInputValue.trim()]);
-            } else {
-                alert('Please add the color first before adding the color code');
-            }
-            setColorHexInputValue('');
-        }
-    }
-
-    const handleAddFrontImage = (event) => {
-        if (event.key === 'Enter' && productFrontImagesInputValue.trim() !== '') {
-            setProductFrontImages([...productFrontImages, productFrontImagesInputValue.trim()]);
-            setProductFrontImagesInputValue('');
-        }
-    }
-
-    const handleAddBackImage = (event) => {
-        if (event.key === 'Enter' && productBackImagesInputValue.trim() !== '') {
-            setProductBackImages([...productBackImages, productBackImagesInputValue.trim()]);
-            setProductBackImagesInputValue('');
-        }
-    }
-
-    const handleDeleteColor = (colorToDelete) => {
-        setColors(colors.filter(color => color !== colorToDelete));
-    };
-
-    const handleDeleteColorHex = (colorToDelete) => {
-        setColorCodes(colorCodes.filter(color => color !== colorToDelete));
-    }
-
-    const handleDeleteFrontImage = (imageToDelete) => {
-        setProductFrontImages(productFrontImages.filter(image => image !== imageToDelete));
-    }
-
-    const handleDeleteBackImage = (imageToDelete) => {
-        setProductBackImages(productBackImages.filter(image => image !== imageToDelete));
-    }
-
+    const [loading, setLoading] = React.useState(false);
+   
     const handleRatingChange = (event) => {
         setRating(event.target.value);
-    }
-
-    const handleSizeRangeBottomChange = (event) => {
-        setSizeRangeBottom(event.target.value);
-    }
-
-    const handleSizeRangeTopChange = (event) => {
-        setSizeRangeTop(event.target.value);
     }
 
     const handleTagChange = (event) => {
@@ -105,36 +38,39 @@ function Admin() {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        if (!name || !vendor || !description || !sizeRangeBottom || !sizeRangeTop || !priceRangeBottom || !priceRangeTop || colors.length === 0 || colorCodes.length === 0 || productFrontImages.length === 0 || productBackImages.length === 0 || !rating || !tag || !category || !type) {
+        if (!styleCode || !priceRangeBottom || !priceRangeTop || !rating || !tag || !category || !type) {
             alert('Please fill out all fields');
             return;
         }
         const product = {
-            name,
-            vendor,
-            description,
-            sizeRangeBottom,
-            sizeRangeTop,
+            styleCode,
             priceRangeBottom,
             priceRangeTop,
-            colors,
-            colorCodes,
-            productFrontImages,
-            productBackImages,
             rating,
             tag,
             category,
             type
         }
-        
-        const res = await axios.post(config.backendUrl+'/api/products/add', product);
-        if (res.data) {
-            alert('Product added successfully');
-            //scroll to top of page
-            //window.location.reload();
-        }
-        else
+        try {
+            setLoading(true)
+            const res = await axios.post(config.backendUrl+'/api/products/add', product);
+            if (res.data) {
+                alert('Product added successfully');
+                //scroll to top of page
+                //window.location.reload();
+            }
+            else {
+                alert('Product could not be added');
+            }
+            setLoading(false)
+        } catch (err) {
+            console.error(err);
             alert('Product could not be added');
+            setLoading(false)
+        } finally {
+            setLoading(false)
+        }
+        
     }
 
     const handleKeyPress = (event) => {
@@ -142,6 +78,8 @@ function Admin() {
             event.preventDefault();
         }
     }
+
+
 
   return (
     <Box bgcolor="#f5f5f5" pt={6} pb={8} display="flex" justifyContent="center">
@@ -160,51 +98,10 @@ function Admin() {
             </Typography>
             <Stack display="flex" justifyContent="center" alignItems="center" width={mobile ? '92%' : '40vw'} spacing={2} mt={mobile ? 2 : 4}>
                 <Box width="100%">
-                    <Typography variant="h6">Name</Typography>
-                    <TextField value={name} onChange={(e)=>setName(e.target.value)} id="outlined-basic" variant="outlined" fullWidth={true} size={mobile ? "small" : "medium"} required/>
+                    <Typography variant="h6">Style Code</Typography>
+                    <TextField value={styleCode} onChange={(e)=>setStyleCode(e.target.value)} id="outlined-basic" variant="outlined" fullWidth={true} size={mobile ? "small" : "medium"} required/>
                 </Box>
-                <Box width="100%">
-                    <Typography variant="h6">Vendor</Typography>
-                    <TextField value={vendor} onChange={(e)=>setVendor(e.target.value)} id="outlined-basic" variant="outlined" fullWidth={true} size={mobile ? "small" : "medium"} required/>
-                </Box>
-                <Box width="100%">
-                    <Typography variant="h6">Description</Typography>
-                    <TextField value={description} onChange={(e)=>setDescription(e.target.value)} id="outlined-basic" variant="outlined" fullWidth={true} size={mobile ? "small" : "medium"} required/>
-                </Box>
-                <Box width="100%">
-                    <Typography variant="h6">Size Range</Typography>
-                    <Stack direction="row" width="100%" spacing={2} alignItems="center">
-                        <FormControl>
-                            <Select value={sizeRangeBottom} onChange={handleSizeRangeBottomChange}>
-                                <MenuItem value={'2XS'}>2XS</MenuItem>
-                                <MenuItem value={'XS'}>XS</MenuItem>
-                                <MenuItem value={'S'}>S</MenuItem>
-                                <MenuItem value={'M'}>M</MenuItem>
-                                <MenuItem value={'L'}>L</MenuItem>
-                                <MenuItem value={'XL'}>XL</MenuItem>
-                                <MenuItem value={'2XL'}>2XL</MenuItem>
-                                <MenuItem value={'3XL'}>3XL</MenuItem>
-                                <MenuItem value={'4XL'}>4XL</MenuItem>
-                                <MenuItem value={'5XL'}>5XL</MenuItem>
-                            </Select>
-                        </FormControl>
-                        <Typography variant="h6">-</Typography>
-                        <FormControl>
-                            <Select value={sizeRangeTop} onChange={handleSizeRangeTopChange}>
-                                <MenuItem value={'2XS'}>2XS</MenuItem>
-                                <MenuItem value={'XS'}>XS</MenuItem>
-                                <MenuItem value={'S'}>S</MenuItem>
-                                <MenuItem value={'M'}>M</MenuItem>
-                                <MenuItem value={'L'}>L</MenuItem>
-                                <MenuItem value={'XL'}>XL</MenuItem>
-                                <MenuItem value={'2XL'}>2XL</MenuItem>
-                                <MenuItem value={'3XL'}>3XL</MenuItem>
-                                <MenuItem value={'4XL'}>4XL</MenuItem>
-                                <MenuItem value={'5XL'}>5XL</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </Stack>
-                </Box>
+                
                 <Box width="100%">
                     <Typography variant="h6">Price Range ($)</Typography>
                     <Stack direction="row" width="100%" spacing={2} alignItems="center">
@@ -212,94 +109,6 @@ function Admin() {
                         <Typography variant="h6">-</Typography>
                         <TextField value={priceRangeTop} type="number" onChange={(e)=>setPriceRangeTop(e.target.value)} id="outlined-basic" variant="outlined" size={mobile ? "small" : "medium"} required/>
                     </Stack>
-                </Box>
-                <Box width="100%">
-                    <Typography variant="h6">Colors</Typography>
-                    <TextField
-                        value={colorInputValue}
-                        onChange={(e) => setColorInputValue(e.target.value)}
-                        onKeyPress={handleAddColor}
-                        id="outlined-basic"
-                        variant="outlined"
-                        fullWidth={true}
-                        size={mobile ? "small" : "medium"}
-                    />
-                    <Box>
-                        {colors.map((color, index) => (
-                            <Chip
-                                key={index}
-                                label={color}
-                                onDelete={() => handleDeleteColor(color)}
-                                style={{ margin: '4px' }}
-                            />
-                        ))}
-                    </Box>
-                </Box>
-                <Box width="100%">
-                    <Typography variant="h6">Color Codes</Typography>
-                    <TextField
-                        value={colorHexInputValue}
-                        onChange={(e) => setColorHexInputValue(e.target.value)}
-                        onKeyPress={handleAddColorHex}
-                        id="outlined-basic"
-                        variant="outlined"
-                        fullWidth={true}
-                        size={mobile ? "small" : "medium"}
-                    />
-                    <Box>
-                        {colorCodes.map((color, index) => (
-                            <Chip
-                                key={index}
-                                label={colors[index] + ' - ' +  color}
-                                onDelete={() => handleDeleteColorHex(color)}
-                                style={{ margin: '4px' }}
-                            />
-                        ))}
-                    </Box>
-                </Box>
-                <Box width="100%">
-                    <Typography variant="h6">Product Front Images (URLs)</Typography>
-                    <TextField
-                        value={productFrontImagesInputValue}
-                        onChange={(e) => setProductFrontImagesInputValue(e.target.value)}
-                        onKeyPress={handleAddFrontImage}
-                        id="outlined-basic"
-                        variant="outlined"
-                        fullWidth={true}
-                        size={mobile ? "small" : "medium"}
-                    />
-                    <Box>
-                        {productFrontImages.map((url, index) => (
-                            <Chip
-                                key={index}
-                                label={url}
-                                onDelete={() => handleDeleteFrontImage(url)}
-                                style={{ margin: '4px' }}
-                            />
-                        ))}
-                    </Box>
-                </Box>
-                <Box width="100%">
-                    <Typography variant="h6">Product Back Images (URLs)</Typography>
-                    <TextField
-                        value={productBackImagesInputValue}
-                        onChange={(e) => setProductBackImagesInputValue(e.target.value)}
-                        onKeyPress={handleAddBackImage}
-                        id="outlined-basic"
-                        variant="outlined"
-                        fullWidth={true}
-                        size={mobile ? "small" : "medium"}
-                    />
-                    <Box>
-                        {productBackImages.map((url, index) => (
-                            <Chip
-                                key={index}
-                                label={url}
-                                onDelete={() => handleDeleteBackImage(url)}
-                                style={{ margin: '4px' }}
-                            />
-                        ))}
-                    </Box>
                 </Box>
                 <Stack direction="row" width="100%" spacing={2} alignItems="center">
                     <Box>
@@ -351,7 +160,10 @@ function Admin() {
                         </FormControl>
                     </Box>
                 </Stack>
-                <Button variant="contained" color="primary" fullWidth size={mobile ? "small" : "large"} type="submit">Submit</Button>
+                <Button variant="contained" color="primary" fullWidth disabled={loading}
+                size={mobile ? "small" : "large"} type="submit">
+                    Submit
+                </Button>
             </Stack>
             </form>
         </Box>
