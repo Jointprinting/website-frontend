1467bf7b4779c495d3a9d36fe24898aed894abc2
nathanielshochat2021 <35067618+nathanielshochat2021@users.noreply.github.com>
2025-11-29 16:28:52 -0500

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,3 +1,4 @@
+// src/screens/Home.js
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import ProductCategories from '../modules/views/ProductCategories';
@@ -5,7 +6,6 @@ import ProductSmokingHero from '../modules/views/ProductSmokingHero';
 import ProductHero from '../modules/views/ProductHero';
 import ProductValues from '../modules/views/ProductValues';
 import ProductHowItWorks from '../modules/views/ProductHowItWorks';
-import ProductCTA from '../modules/views/ProductCTA';
 
 function Home() {
   return (
@@ -14,7 +14,6 @@ function Home() {
       <ProductValues />
       <ProductCategories />
       <ProductHowItWorks />
-      <ProductCTA />
       <ProductSmokingHero />
     </Box>
   );
